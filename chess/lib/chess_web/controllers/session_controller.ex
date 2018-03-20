0a40a328bcf2d0c17fcb3ae0edcf925b81ced082
William <coconuts1040@gmail.com>
2018-03-20 19:16:57 -0400

    user sessions created

---
+++ chess/lib/chess_web/controllers/session_controller.ex
@@ -0,0 +1,27 @@
+#Taken from Nat Tuck's lecture notes
+defmodule ChessWeb.SessionController do
+  use ChessWeb, :controller
+
+  alias Chess.Accounts
+
+  def create(conn, %{"email" => email}) do
+    user = Accounts.get_user_via_email(email)
+    if user do
+      conn
+      |> put_session(:user_id, user.id)
+      |> put_flash(:info, "Logged in as #{user.name}")
+      |> redirect(to: "/users") #change to redirect to /games
+    else
+      conn
+      |> put_flash(:error, "Incorrect login")
+      |> redirect(to: page_path(conn, :index))
+    end
+  end
+
+  def delete(conn, _params) do
+    conn
+    |> delete_session(:user_id)
+    |> put_flash(:info, "Logged out")
+    |> redirect(to: page_path(conn, :index))
+  end
+end
