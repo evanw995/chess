a5d52de7f78dabca5dcba9356c5bc9ade7b78900
Evan Welsh <evanw995@gmail.com>
2018-03-24 17:51:46 -0400

    initial game page commit

---
+++ chess/assets/js/gamePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ChessGame } from './ChessGame.jsx';
+
+export default function run_gamePage(root, channel) {
+  ReactDOM.render(<GamePage channel={channel}/>, root);
+}
+
+class GamePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      channel: props.channel,
+    };
+  }
+
+  render() {
+    return (
+      <div id="chessGame">
+        <ChessGame channel={this.props.channel}/>
+      </div>
+    );
+  }
+}
